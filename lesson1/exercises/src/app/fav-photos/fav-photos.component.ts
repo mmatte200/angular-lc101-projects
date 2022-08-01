daa97ab876ada3c0ae89061498926cb8b17a2169
Mattei <michael_mattei@cable.comcast.com>
2022-08-01 16:00:18 -0400

    fonts added and pictures added/styled

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Rad Pics';
+  image1 = 'https://m.media-amazon.com/images/M/MV5BMTcxNDM3OTMzOV5BMl5BanBnXkFtZTcwODYxMDIyNw@@._V1_.jpg';
+  image2 = 'https://images-ext-1.discordapp.net/external/nurB_3MiF9T_s3pU2udmHjmzo2qqoEyZAdMuRFeqpTM/%3Fv%3D1573618694/https/cdn.shopify.com/s/files/1/0057/3728/3618/products/20664117398ad7301d9a9af6d2e5aa5c_1e3ea98b-b962-4982-9f74-2e44381fc6ff_480x.progressive.jpg?width=444&height=686';
+  image3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKpttnc6TWURzqd4SIo5YSPjWV4QgcVoHfdg&usqp=CAU';
 
   constructor() { }
 
